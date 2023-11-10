de17a164a849600bbfee7d9a47118d6edefadbd7
Brent Yarger <brent.yarger@konghq.com>
2023-11-09 17:08:43 -0800

    normalize prefix path before generating runtime hash, update dockerfiles

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -963,6 +963,7 @@ end
 _M.topological_sort = topological_sort
 
 function _M.get_runtime_data_path(prefix)
+  local prefix = pl_path.normpath(prefix)
   -- Path used for runtime data such as unix domain sockets
   local prefix_hash = string.sub(ngx.md5(prefix), 1, 7)
   return fmt("/var/run/kong/%s", prefix_hash)
